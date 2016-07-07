f8f343feb4141c0d199b796771201afd352b6dbd
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-07-07 18:54:20 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shuffle Letters
+
+This is the library where [Shuffle Letters Effect](http://tutorialzine.com/2011/09/shuffle-letters-effect-jquery/) was rewritten in vanilla javascript.
---
+++ src/shuffle-letters.js
@@ -0,0 +1,87 @@
+'use strict';
+
+function range(start, end) {
+  return Array.from({length: end - start + 1}, (_, i) => i + start);
+}
+
+function arrayMap(el, fn) {
+  const elArray = (
+    Array.isArray(el) || el instanceof NodeList || el instanceof HTMLCollection
+  )
+    ? Array.from(el)
+    : [el];
+  return elArray.map(fn);
+}
+
+function getRandomChar(type) {
+  let pool = '';
+  switch (type) {
+    case 'lowerLetter':
+      pool = 'abcdefghijklmnopqrstuvwxyz0123456789';
+      break;
+    case 'upperLetter':
+      pool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      break;
+    case 'symbol':
+      pool = ',.?/\\(^)![]{}*&^%$#\'"';
+      break;
+  }
+  const poolArray = Array.from(pool);
+  return poolArray[Math.floor(Math.random() * poolArray.length)];
+}
+
+function shuffleLetters(els, options) {
+  options = Object.assign({}, {
+    step: 8,
+    fps: 25,
+    text: ''
+  }, options);
+
+  return Promise.all(arrayMap(els, (el) => new Promise((resolve) => {
+    if (el.dataset.shuffleLettersAnimated === 'true') {
+      return;
+    }
+    el.dataset.shuffleLettersAnimated = 'true';
+
+    const strArray = Array.from(options.text || el.textContent);
+    const types = [];
+    const letters = [];
+
+    strArray.forEach((char, i) => {
+      if (char === ' ') {
+        types[i] = 'space';
+        return;
+      } else if (/[a-z]/.test(char)) {
+        types[i] = 'lowerLetter';
+      } else if (/[A-Z]/.test(char)) {
+        types[i] = 'upperLetter';
+      } else {
+        types[i] = 'symbol';
+      }
+      letters.push(i);
+    });
+    el.textContent = '';
+
+    (function shuffle(start) {
+      const len = letters.length;
+      const shuffledArray = [].concat(strArray);
+
+      if (start > len) {
+        el.dataset.shuffleLettersAnimated = 'false';
+        return resolve(el);
+      }
+
+      range(Math.max(start, 0), len).forEach((i) => {
+        shuffledArray[letters[i]] = i < (start + options.step)
+          ? getRandomChar(types[letters[i]])
+          : '';
+      });
+      el.textContent = shuffledArray.join('');
+
+      setTimeout(() => shuffle(start + 1), 1000 / options.fps);
+    })(-options.step);
+  })))
+    .then(() => els);
+}
+
+module.exports = shuffleLetters;
